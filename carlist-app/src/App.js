0e7115637b9f61582fd48c99ea1c8646eb1fa797
stella-maris <mango.gal@gmail.com>
2019-01-27 18:51:35 -0800

    creates basic list

---
+++ carlist-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import CarListItem from './CarListItem';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        < CarListItem/>
+      </div>
+    );
+  }
+}
+
+export default App;
